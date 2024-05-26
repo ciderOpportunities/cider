99f94059a0f2eb989b6415d3f7b7e565036ac59b
Carey Manwa <okal@dhcp-vl2041-5488.eduroam.cornell.edu>
2024-05-26 00:57:36 -0400

    emhanced site's look and added dark  mode toggle

--- scripts/dark-mode.js
+++ scripts/dark-mode.js
@@ -5,12 +5,13 @@ $(document).ready(function() {
         $('.navbar').toggleClass('dark-mode');
         $('.nav-link').toggleClass('dark-mode');
         $('.sidebar').toggleClass('dark-mode');
-        $('.container').toggleClass('dark-mode');
+        $('.content-container').toggleClass('dark-mode');
         $('footer').toggleClass('dark-mode');
         $('.opportunity-card').toggleClass('dark-mode');
         $('.footer-logo-container').toggleClass('dark-mode');
         $('.footer-section').toggleClass('dark-mode');
         $('.footer-bottom').toggleClass('dark-mode');
+        $('.job-count').toggleClass('dark-mode');
     });
 
     // Scroll to Top Button
@@ -28,4 +29,14 @@ $(document).ready(function() {
         $('html, body').animate({ scrollTop: 0 }, 600);
         return false;
     });
+
+    // Job Count
+    function updateJobCount() {
+        const jobCount = $('.opportunity-card').length;
+        $('#jobCount').text(jobCount);
+    }
+
+    // Update job count on page load
+    updateJobCount();
+
 });
