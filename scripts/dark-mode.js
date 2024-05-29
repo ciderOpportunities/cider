f5d394286de509aaa68135c3c1ffb7dcbae0ef32
Carey Manwa <okal@dhcp-vl2041-5488.eduroam.cornell.edu>
2024-05-29 05:34:27 -0400

    Updated the darkmode icon

--- scripts/dark-mode.js
+++ scripts/dark-mode.js
@@ -1,6 +1,6 @@
-$(document).ready(function() {
+$(document).ready(function () {
     // Dark Mode Toggle
-    $('#darkModeToggle').on('change', function() {
+    $('#darkModeToggle').on('change', function () {
         $('body').toggleClass('dark-mode');
         $('.navbar').toggleClass('dark-mode');
         $('.nav-link').toggleClass('dark-mode');
@@ -19,4 +19,23 @@ $(document).ready(function() {
         $('.dropdown-menu').toggleClass('dark-mode');
         $('.navbar-brand').toggleClass('dark-mode');
     });
+    document.getElementById('darkModeToggle').addEventListener('change', function () {
+        const darkModeIcon = document.getElementById('darkModeIcon');
+        if (this.checked) {
+            darkModeIcon.classList.remove('bi-moon-fill');
+            darkModeIcon.classList.add('bi-sun-fill');
+
+        } else {
+            darkModeIcon.classList.remove('bi-sun-fill');
+            darkModeIcon.classList.add('bi-moon-fill');
+
+        }
+    });
+
+
+
 });
+
+
+
+
